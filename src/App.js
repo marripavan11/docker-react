d77ffd329017ed769eb9020416ea0919677fe45f
pavan_marri <pavan.marri@phenompeople.com>
2020-06-26 15:25:24 +0530

    updated the source file

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Yo yo  !
+          This is all new ME  !
         </p>
         <a
           className="App-link"
